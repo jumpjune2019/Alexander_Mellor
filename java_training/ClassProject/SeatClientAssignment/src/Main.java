dd0d9f51ac0a19ceaeed347e159ae2cbe3c277e9
Alexander Mellor <alexmellor2014@gmai.com>
2019-07-17 11:32:55 -0500

    Adding Files/Updating Files

--- java_training/ClassProject/SeatClientAssignment/src/Main.java
+++ java_training/ClassProject/SeatClientAssignment/src/Main.java
@@ -1,9 +1,8 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
 }
